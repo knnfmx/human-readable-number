1f1a7d6b7dc08f9e9d361cbdd598c141df119474
kennyfmx <fmxhighlife@gmail.com>
2021-03-02 17:46:06 +0300

    master

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+    let zero = "zero";
+	let units = ["one","two","three","four","five","six","seven","eight","nine"];
+	let teen = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+	let ty = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+	
+	// for zero
+	if (num === 0) {
+		return zero;
+	}
 }
