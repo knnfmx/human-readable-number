e4402e318e23afeb71dcb8e7a00236979d20431e
kennyfmx <fmxhighlife@gmail.com>
2021-03-02 21:32:54 +0300

    fix: hundred part

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ module.exports = function toReadable (number) {
 	let units = ["one","two","three","four","five","six","seven","eight","nine"];
 	let teen = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
 	let ty = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
-	
 	// for zero
-	// for zero
+
 	if (number === 0) {
 		return zero;
 	}
-	// from 100 to 1000 
-	if (number > 99 && number < 1000) {
+
+    // for hundreds 
+
+	if (number % 100 == 0) {
+		return `${units[Math.floor(number / 100 - 1)]} hundred`;
+	} else if (number > 99 && number < 1000) {
 		return `${units[Math.floor(number / 100 - 1)]} hundred ${numsToWords(number - (Math.floor(number / 100)) * 100)}`;
-	} else return numsToWords(number - (Math.floor(number / 100)) * 100);
+	} else {
+		return numsToWords(number - (Math.floor(number / 100)) * 100);
+	}
 
 	// from 1 to 19 and for all ty nums
+    
 	function numsToWords(number) {
 		if (number <= 9) {
 			return units[number - 1];
