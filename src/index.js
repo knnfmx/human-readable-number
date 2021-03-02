bf94c8007aedda13e5bdd53e064307cc931c92a0
kennyfmx <fmxhighlife@gmail.com>
2021-03-02 20:25:25 +0300

    feat: final result

--- src/index.js
+++ src/index.js
@@ -5,22 +5,23 @@ module.exports = function toReadable (number) {
 	let ty = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
 	
 	// for zero
+	// for zero
 	if (number === 0) {
 		return zero;
 	}
 	// from 100 to 1000 
-	if (number > 99) {
-		return units[Math.floor(number / 100 - 1)] + " hundred" + " " + numsToWords(number - (Math.floor(number / 100)) * 100);
+	if (number > 99 && number < 1000) {
+		return `${units[Math.floor(number / 100 - 1)]} hundred ${numsToWords(number - (Math.floor(number / 100)) * 100)}`;
 	} else return numsToWords(number - (Math.floor(number / 100)) * 100);
 
 	// from 1 to 19 and for all ty nums
-	function numsToWords() {
+	function numsToWords(number) {
 		if (number <= 9) {
 			return units[number - 1];
 		} else if (number <= 19) {
 			return teen[number - 10];
 		} else {
-			return ty[Math.floor(number / 10) - 2] + " " + units[number % 10 - 1];
+			return `${ty[Math.floor(number / 10) - 2]} ${units[number % 10 - 1]}`;
 		}
 	}
 }
