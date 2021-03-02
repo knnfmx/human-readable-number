b7fae7fabc6097009407fc001d0ad02afff1682b
kennyfmx <fmxhighlife@gmail.com>
2021-03-02 18:27:17 +0300

    master

--- src/index.js
+++ src/index.js
@@ -8,4 +8,14 @@ module.exports = function toReadable (number) {
 	if (number === 0) {
 		return zero;
 	}
+    
+    // from 1 to 19 and for all ty nums
+
+	if (num <= 9) {
+		return units[num - 1];
+	} else if (num <= 19) {
+		return teen[num - 10];
+	} else {
+		return ty[Math.floor(num / 10) - 2];
+	}
 }
